08c18bcc8d5e1503e7aa1894eaa7486f479ab293
PatrikStrausz <patkolo1244@gmail.com>
2021-03-06 19:43:02 +0100

    Biome 3 done

---
+++ JumpGame/Assets/Scripts/SpaceDustManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceDustManager : MonoBehaviour
+{
+
+    public GameObject dustSmall;
+    public GameObject dustBig;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            dustSmall.SetActive(true);
+            dustBig.SetActive(true);
+
+           
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            dustSmall.SetActive(false);
+            dustBig.SetActive(false);
+
+        }
+    }
+}
